062ecd7f0701ce1b2c28a8962d3d229520ea81f5
marcfervil <marcfervil630@gmail.com>
2020-11-02 23:42:22 -0600

    Ugliest File Browser ever (tm)

--- src/index.js
+++ src/index.js
@@ -42,11 +42,13 @@ ipcMain.on('selectDirectory', async function(event, arg) {
     	if(!result.cancelled){
     		let path = result.filePaths[0];
 	    	console.log("path: "+path);
+
 	    	let metrics = new Metrics(path);
-			event.reply('metrics', metrics.getAll());
+				console.log(metrics);
+			event.reply('metrics', metrics);
 		}
     });
-    
+
 
 });
 
@@ -66,17 +68,6 @@ app.on('activate', () => {
 })
 //https://github.com/foliojs/pdfkit
 
-function test(){
-	let name = "Project/wey-master";
-
-	let metrics = new Metrics(name);
-	console.log("LOC: "+metrics.getLOC());
-	console.log("DIT: "+metrics.getDIT());
-	console.log("NOC: "+metrics.getNOC());
-	console.log("Sum Cyclomatic: "+metrics.getCyclomatic());
-	//console.log("WMC: "+metrics.getWMC("test"));
-
-}
 
 
 console.full = function(obj){
